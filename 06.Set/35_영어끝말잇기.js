63c1c05c38e9af541e08544c62f45ad0c946a561
정채영 <sarah100217@gmail.com>
2024-08-30 12:50:11 +0900

    영어 끝말잇기

---
+++ 06.Set/35_영어끝말잇기.js
@@ -0,0 +1,25 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12981
+
+// 권장시간 40분 / 풀이시간 58분
+function solution(n, words) {
+  const answer = [0, 0];
+
+  for (let i = 0; i < words.length; i++) {
+    const dup = words.indexOf(words[i]) !== i;
+    let wrong = "";
+    if (words[i - 1])
+      wrong = words[i - 1][words[i - 1].length - 1] !== words[i][0];
+    if (dup || wrong || words[i].length <= 1) {
+      if ((i + 1) % n !== 0) {
+        answer[0] = (i + 1) % n;
+        answer[1] = Math.floor((i + 1) / n) + 1;
+      } else {
+        answer[0] = n;
+        answer[1] = (i + 1) / n;
+      }
+      break;
+    }
+  }
+
+  return answer;
+}
